d0f523630ba208b555a5aa25ba6815428f04599d
崔岩 <cuiyan@xiaomi.com>
2018-05-04 15:47:16 +0800

    优化图表滑动处理

--- baseDevKit/src/main/java/com/dev/kit/basemodule/multiChildHistogram/MultiGroupHistogramView.java
+++ baseDevKit/src/main/java/com/dev/kit/basemodule/multiChildHistogram/MultiGroupHistogramView.java
@@ -10,12 +10,14 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.SparseArray;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.Scroller;
 
 import com.dev.kit.basemodule.R;
 import com.dev.kit.basemodule.util.DisplayUtil;
-import com.dev.kit.basemodule.util.LogUtil;
 
 import java.util.List;
 
@@ -61,6 +63,24 @@ public class MultiGroupHistogramView extends View {
     private List<MultiGroupHistogramGroupData> dataList;
     private SparseArray<Float> childMaxValueArray;
 
+    private Scroller scroller;
+    private GestureDetector gestureDetector;
+    private int minimumVelocity;
+    private int maximumVelocity;
+    private final GestureDetector.SimpleOnGestureListener simpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (Math.abs(velocityX) > minimumVelocity) {
+                velocityX *= 1.5;
+                if (Math.abs(velocityX) > maximumVelocity) {
+                    velocityX = maximumVelocity * velocityX / velocityX;
+                }
+                scroller.fling(getScrollX(), getScrollY(), -(int) velocityX, 0, 0, histogramContentWidth + histogramPaddingStart - width, 0, 0);
+            }
+            return true;
+        }
+    };
+
     public MultiGroupHistogramView(Context context) {
         this(context, null);
     }
@@ -95,6 +115,11 @@ public class MultiGroupHistogramView extends View {
         coordinateAxisPaint.setStrokeWidth(coordinateAxisWidth);
         childRect = new Rect();
         childPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        scroller = new Scroller(getContext());
+        gestureDetector = new GestureDetector(getContext(), simpleOnGestureListener);
+        ViewConfiguration configuration = ViewConfiguration.get(getContext());
+        minimumVelocity = configuration.getScaledMinimumFlingVelocity();
+        maximumVelocity = configuration.getScaledMaximumFlingVelocity();
     }
 
     @Override
@@ -128,6 +153,10 @@ public class MultiGroupHistogramView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (!scroller.isFinished()) {
+            scroller.abortAnimation();
+        }
+        gestureDetector.onTouchEvent(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
                 lastX = event.getX();
@@ -148,6 +177,13 @@ public class MultiGroupHistogramView extends View {
             }
         }
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), 0);
+        }
     }
 
     public void setDataList(@NonNull List<MultiGroupHistogramGroupData> dataList) {
@@ -173,7 +209,6 @@ public class MultiGroupHistogramView extends View {
             }
         }
         histogramContentWidth += -groupInterval;
-        LogUtil.e("histogramContentWidth: " + histogramContentWidth);
     }
 
     @Override
