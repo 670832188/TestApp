cc98a13dfc34c40bc1db18f085122d0eacc65368
崔岩 <cuiyan@xiaomi.com>
2018-07-17 09:55:15 +0800

    ts

--- baseDevKit/src/main/java/com/dev/kit/basemodule/view/WaveView.java
+++ baseDevKit/src/main/java/com/dev/kit/basemodule/view/WaveView.java
@@ -2,6 +2,7 @@ package com.dev.kit.basemodule.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
@@ -10,18 +11,30 @@ import android.view.View;
 
 import com.dev.kit.basemodule.util.DisplayUtil;
 
-import java.util.Random;
-
 /**
  * Created by cuiyan on 2018/7/13.
  */
 public class WaveView extends View {
-    private int width = DisplayUtil.getScreenWidth() * 2;
-    private int height = DisplayUtil.dp2px(60);
-    private Paint wavePaint;
-    private Path wavePath;
-    //    private Path wavePath;
-    private boolean isStart;
+    private int width;
+    private int height;
+    private Paint wavePaint1;
+    private Paint wavePaint2;
+    private Paint wavePaint3;
+    private Path wavePath1;
+    private Path wavePath2;
+    private Path wavePath3;
+    private float offsetAngle1 = -(float) Math.PI / 4;
+    private float offsetAngle2 = (float) Math.PI / 2;
+    private float offsetAngle3 = (float) Math.PI / 4 *3;
+    private boolean isRunning;
+    private final Runnable refreshRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (isRunning) {
+                postInvalidate();
+            }
+        }
+    };
 
     public WaveView(Context context) {
         this(context, null);
@@ -37,35 +50,31 @@ public class WaveView extends View {
     }
 
     private void init() {
-        wavePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        wavePaint.setStyle(Paint.Style.STROKE);
-        wavePaint.setStrokeWidth(DisplayUtil.dp2px(2));
-        wavePath = new Path();
-        resetPath();
-    }
+        wavePaint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wavePaint1.setStyle(Paint.Style.STROKE);
+        wavePaint1.setStrokeWidth(DisplayUtil.dp2px(2));
+        wavePaint1.setColor(Color.YELLOW);
+        wavePath1 = new Path();
 
-//    @Override
-//    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-//        width = w;
-//        height = h;
-//        if (width > 0 && height > 0) {
-//            resetPath();
-//        }
-//    }
+        wavePaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wavePaint2.setStyle(Paint.Style.STROKE);
+        wavePaint2.setStrokeWidth(DisplayUtil.dp2px(2));
+        wavePaint2.setColor(Color.BLUE);
+        wavePath2 = new Path();
 
-
-    private void resetPath() {
-        wavePath.reset();
-        wavePath.moveTo(0, height / 2);
-        wavePath.cubicTo(width / 8, height / 4, width / 8 * 3, height / 4 * 3, width / 2, height / 2);
-//        wavePath.moveTo(width / 2, height / 2);
-        wavePath.cubicTo(width / 8 + width / 2, height / 4, width / 8 * 3 + width / 2, height / 4 * 3, width, height / 2);
+        wavePaint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wavePaint3.setStyle(Paint.Style.STROKE);
+        wavePaint3.setStrokeWidth(DisplayUtil.dp2px(2));
+        wavePaint3.setColor(Color.RED);
+        wavePath3 = new Path();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(width, height);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
+//        setMeasuredDimension(width, height);
     }
 
     @Override
@@ -73,27 +82,50 @@ public class WaveView extends View {
         if (width <= 0 || height <= 0) {
             return;
         }
-        canvas.drawPath(wavePath, wavePaint);
-        postDelayed(refreshRunnable, 200);
+        wavePath1.reset();
+        wavePath2.reset();
+        wavePath3.reset();
+        float amplitude = (float) height / 2 - 10;
+        wavePath1.moveTo(0, (float) (Math.sin(offsetAngle1) * amplitude + height / 2));
+
+        for (float i = 0; i < width; i++) {
+            float angle = (float) (i / width * Math.PI * 3) + offsetAngle1;
+            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            wavePath1.lineTo(i, y);
+        }
+        wavePath2.moveTo(0, (float) (Math.sin(offsetAngle2) * amplitude + height / 2));
+        for (float i = 0; i < width; i++) {
+            float angle = (float) (i / width * Math.PI * 3) + offsetAngle2;
+            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            wavePath2.lineTo(i, y);
+        }
+        wavePath3.moveTo(0, (float) (Math.sin(offsetAngle3) * amplitude + height / 2));
+        for (float i = 0; i < width; i++) {
+            float angle = (float) (i / width * Math.PI * 3) + offsetAngle3;
+            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            wavePath3.lineTo(i, y);
+        }
+        if (isRunning) {
+            offsetAngle1 += Math.PI * 0.01;
+            offsetAngle2 -= Math.PI * 0.01;
+            offsetAngle3 += Math.PI * 0.015;
+            postDelayed(refreshRunnable, 10);
+        }
+        canvas.drawPath(wavePath1, wavePaint1);
+        canvas.drawPath(wavePath2, wavePaint2);
+        canvas.drawPath(wavePath3, wavePaint3);
     }
 
-    private final Runnable refreshRunnable = new Runnable() {
-        @Override
-        public void run() {
-            scrollBy(10, 0);
-        }
-    };
-
     public void start() {
-        if (!isStart) {
-            isStart = true;
+        if (!isRunning) {
+            isRunning = true;
             postInvalidate();
         }
     }
 
     public void stop() {
-        if (isStart) {
-            isStart = false;
+        if (isRunning) {
+            isRunning = false;
         }
     }
 }
