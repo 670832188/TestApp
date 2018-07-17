da4f36a3903e4fa689a0f92235a85aba6f0c2eab
崔岩 <cuiyan@xiaomi.com>
2018-07-17 11:32:20 +0800

    ts

--- baseDevKit/src/main/java/com/dev/kit/basemodule/view/WaveView.java
+++ baseDevKit/src/main/java/com/dev/kit/basemodule/view/WaveView.java
@@ -3,6 +3,7 @@ package com.dev.kit.basemodule.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
@@ -10,6 +11,8 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import com.dev.kit.basemodule.util.DisplayUtil;
+
+import java.util.Random;
 
 /**
  * Created by cuiyan on 2018/7/13.
@@ -25,7 +28,8 @@ public class WaveView extends View {
     private Path wavePath3;
     private float offsetAngle1 = -(float) Math.PI / 4;
     private float offsetAngle2 = (float) Math.PI / 2;
-    private float offsetAngle3 = (float) Math.PI / 4 *3;
+    private float offsetAngle3 = (float) Math.PI ;
+    private float amplitude;
     private boolean isRunning;
     private final Runnable refreshRunnable = new Runnable() {
         @Override
@@ -55,6 +59,8 @@ public class WaveView extends View {
         wavePaint1.setStrokeWidth(DisplayUtil.dp2px(2));
         wavePaint1.setColor(Color.YELLOW);
         wavePath1 = new Path();
+//        LinearGradient gradient = new LinearGradient()
+//        wavePaint1.setS
 
         wavePaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
         wavePaint2.setStyle(Paint.Style.STROKE);
@@ -74,8 +80,10 @@ public class WaveView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         width = getMeasuredWidth();
         height = getMeasuredHeight();
-//        setMeasuredDimension(width, height);
+        amplitude = height / 2 - wavePaint1.getStrokeWidth();
     }
+
+    Random random = new Random();
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -85,30 +93,31 @@ public class WaveView extends View {
         wavePath1.reset();
         wavePath2.reset();
         wavePath3.reset();
-        float amplitude = (float) height / 2 - 10;
-        wavePath1.moveTo(0, (float) (Math.sin(offsetAngle1) * amplitude + height / 2));
-
+        float tempAmplitude = amplitude;
+        wavePath1.moveTo(0, (float) (Math.sin(offsetAngle1) * tempAmplitude + height / 2));
         for (float i = 0; i < width; i++) {
             float angle = (float) (i / width * Math.PI * 3) + offsetAngle1;
-            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            float y = (float) (Math.sin(angle) * tempAmplitude + height / 2);
             wavePath1.lineTo(i, y);
         }
-        wavePath2.moveTo(0, (float) (Math.sin(offsetAngle2) * amplitude + height / 2));
+        tempAmplitude = amplitude - Math.min(DisplayUtil.dp2px(3), height / 5);
+        wavePath2.moveTo(0, (float) (Math.sin(offsetAngle2) * tempAmplitude + height / 2));
         for (float i = 0; i < width; i++) {
             float angle = (float) (i / width * Math.PI * 3) + offsetAngle2;
-            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            float y = (float) (Math.sin(angle) * tempAmplitude + height / 2);
             wavePath2.lineTo(i, y);
         }
-        wavePath3.moveTo(0, (float) (Math.sin(offsetAngle3) * amplitude + height / 2));
+        tempAmplitude = amplitude - 2 * Math.min(DisplayUtil.dp2px(3), height / 5);
+        wavePath3.moveTo(0, (float) (Math.sin(offsetAngle3) * tempAmplitude + height / 2));
         for (float i = 0; i < width; i++) {
             float angle = (float) (i / width * Math.PI * 3) + offsetAngle3;
-            float y = (float) (Math.sin(angle) * amplitude + height / 2);
+            float y = (float) (Math.sin(angle) * tempAmplitude + height / 2);
             wavePath3.lineTo(i, y);
         }
         if (isRunning) {
-            offsetAngle1 += Math.PI * 0.01;
-            offsetAngle2 -= Math.PI * 0.01;
-            offsetAngle3 += Math.PI * 0.015;
+            offsetAngle1 -= Math.PI * 0.03;
+            offsetAngle2 -= Math.PI * 0.02;
+            offsetAngle3 -= Math.PI * 0.01;
             postDelayed(refreshRunnable, 10);
         }
         canvas.drawPath(wavePath1, wavePaint1);
